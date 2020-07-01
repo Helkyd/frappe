9f4c547bda6db9b38958e27d80ee8f455afce7aa
Helkyd <hcesar@gmail.com>
2020-07-01 10:41:53 +0100

    Update reload_and_rename_view_log.py

--- frappe/patches/v11_0/reload_and_rename_view_log.py
+++ frappe/patches/v11_0/reload_and_rename_view_log.py
@@ -2,27 +2,39 @@ from __future__ import unicode_literals
 import frappe
 
 def execute():
-	if frappe.db.exists('DocType', 'View log'):
-		# for mac users direct renaming would not work since mysql for mac saves table name in lower case
-		# so while renaming `tabView log` to `tabView Log` we get "Table 'tabView Log' already exists" error
-		# more info https://stackoverflow.com/a/44753093/5955589 ,
-		# https://dev.mysql.com/doc/refman/8.0/en/server-system-variables.html#sysvar_lower_case_table_names
+	#if frappe.db.exists('DocType', 'View log'):
+	if frappe.database.database.Database.table_exists(frappe.db,'View log'):
 
-		# here we are creating a temp table to store view log data
-		frappe.db.sql("CREATE TABLE `ViewLogTemp` AS SELECT * FROM `tabView log`")
+            # for mac users direct renaming would not work since mysql for mac saves table name in lower case
+            # so while renaming `tabView log` to `tabView Log` we get "Table 'tabView Log' already exists" error
+            # more info https://stackoverflow.com/a/44753093/5955589 ,
+            # https://dev.mysql.com/doc/refman/8.0/en/server-system-variables.html#sysvar_lower_case_table_names
 
-		# deleting old View log table
-		frappe.db.sql("DROP table `tabView log`")
-		frappe.delete_doc('DocType', 'View log')
+            # here we are creating a temp table to store view log data
+            frappe.db.sql("CREATE TABLE `ViewLogTemp` AS SELECT * FROM `tabView log`")
 
-		# reloading view log doctype to create `tabView Log` table
-		frappe.reload_doc('core', 'doctype', 'view_log')
+            # deleting old View log table
+            frappe.db.sql("DROP table `tabView log`")
+            frappe.delete_doc('DocType', 'View log')
 
-		# Move the data to newly created `tabView Log` table
-		frappe.db.sql("INSERT INTO `tabView Log` SELECT * FROM `ViewLogTemp`")
-		frappe.db.commit()
+	elif frappe.database.database.Database.table_exists(frappe.db,'View Log'): 
+		#here we are creating a temp table to store view log data
+            frappe.db.sql("CREATE TABLE `ViewLogTemp` AS SELECT * FROM `tabView Log`")
 
-		# Delete temporary table
-		frappe.db.sql("DROP table `ViewLogTemp`")
-	else:
-		frappe.reload_doc('core', 'doctype', 'view_log')
+
+            # deleting old View log table
+            frappe.db.sql("DROP table `tabView Log`")
+            frappe.delete_doc('DocType', 'View Log')
+
+	# reloading view log doctype to create `tabView Log` table
+	frappe.reload_doc('core', 'doctype', 'view_log')
+
+	# Move the data to newly created `tabView Log` table
+	frappe.db.sql("INSERT INTO `tabView Log` SELECT * FROM `ViewLogTemp`")
+	frappe.db.commit()
+
+	# Delete temporary table
+	frappe.db.sql("DROP table `ViewLogTemp`")
+
+	#else:
+	#       frappe.reload_doc('core', 'doctype', 'view_log')
